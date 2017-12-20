5fa80df7ba08fbd5348a0c42fee48622407acd7d
Anwana Ntofon <anwanantofon@gmail.com>
2017-12-19 19:58:37 -0800

    Starting to work on ListManager component

---
+++ src/components/ListManager.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var List = require('./component/List.jsx');
+
+var ListManager = React.createClass({
+  getInitialState: function(){
+    return {itmes: [],
+newItemText:''};
+},
+  render: function(){
+    <
+
+
+  }
+})
+
+
+<List items={this.state.items}/>
